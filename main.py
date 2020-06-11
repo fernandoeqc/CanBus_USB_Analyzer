c1e8142da4a5671e346006d6052bdcbc7a3c654d
fernandoeqc <fernandoeqc@live.com>
2020-06-11 17:01:46 -0300

    fim do dia
    funcionando

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
-python 
-m1 = Message(data = [0xaa, 0xdd, 3, 4, 5],arbitration_id = 547,is_extended_id=False)
+import robotell_lib as roblib
 
-rob = robotellBus("COM13")
+m1 = roblib.Message(data = [0xaa, 0xdd, 3, 4, 5],arbitration_id = 547,is_extended_id=False)
+
+rob = roblib.robotellBus("COM13")
 
 
 dados = [1,3,5,7,8]
@@ -17,29 +18,29 @@ print("auto mgmt")
 rob.set_auto_bus_management(1)
 
 
-print("le baud")
-config = rob._readconfig(0x01FFFED0,2)
+#print("le baud")
+#config = rob._readconfig(0x01FFFED0,2)
 
 for i in config:
-	print("{:02X}".format(i),end="-")
+    print("{:02X}".format(i),end="-")
 
 input()
 
 cont = 0
 
 while 1:
-	#comandos()
+    #comandos()
 
-	msg = rob._recv_internal(None)
-	if msg != None:
-		mostra = msg.data
-		for i in mostra:
-			#print(mostra)
-			print("{:02X}".format(i),end="-")
-		print("\n")	
-	else:
-		print("None")
-		time.sleep(1)
+    msg = rob._recv_internal(None)
+    if msg != None:
+        mostra = msg.data
+        for i in mostra:
+            #print(mostra)
+            print("{:02X}".format(i),end="-")
+        print("\n")	
+    else:
+        print("None")
+        time.sleep(1)
 print("Execução terminada. Aperte qualquer tecla para fechar...")
 input()
 
